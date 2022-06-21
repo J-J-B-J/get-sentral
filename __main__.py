b3adb9770924e753059ac73dce4f69e8fca4ac0a
SuperHarmony910 <57850338+SuperHarmony910@users.noreply.github.com>
2022-06-21 23:39:38 +1000

    updated code to selenium v4

--- __main__.py
+++ __main__.py
@@ -1,18 +1,21 @@
 # Login using Selenium WebDriver
+import time
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+import os
+from webdriver_manager.core.utils import ChromeType
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 options = Options()
 # Chrome web driver
-from webdriver_manager.chrome import ChromeDriverManager
 
 # Environment variable libraries
-import os
-from dotenv import load_dotenv
 # HTML scraping library
-from bs4 import BeautifulSoup
 # Sleep lib
-import time
 
 load_dotenv()
 # Get credentials
@@ -26,30 +29,35 @@ options.add_argument('log-level=3')
 # options.add_argument("--disable-gpu")
 # options.add_argument("--disable-extensions")
 
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+driver = webdriver.Chrome(options=options,service=Service(ChromeDriverManager(
+    chrome_type=ChromeType.CHROMIUM).install()))
 # URL
 url = "https://caringbahhs.sentral.com.au/portal2/#!/login"
 
 driver.get(url)
 
 # Get timetable once in Sentral dashboard
+
+
 def scrape_timetable(html):
     # Fetch the page and create a BeautifulSoup object
     soup = BeautifulSoup(html, 'html.parser')
     # timetable = soup(lambda tag: tag.name == 'td' and tag.get('class') == ['timetable-dayperiod'])
     # get the child 'strong' in the below asjdjkad              | down here `and tag.get('class') == ['small-caps']`
     # timetable = soup(lambda tag: tag.name in ['strong', 'small'] and tag.parent.parent.attrs.get('class') == 'timetable-class')
-    timetable = soup.find_all(class_="timetable-class") # ^^ turn lambda func code to vanilla function
+    # ^^ turn lambda func code to vanilla function
+    timetable = soup.find_all(class_="timetable-class")
     print(timetable)
     # print(driver.find_elements(By.CLASS_NAME, "small-caps"))
     print('ABC')
+
 
 # Check if you already are logged in and in the dashboard
 if driver.current_url == 'https://caringbahhs.sentral.com.au/portal/dashboard':
     scrape_timetable(driver.current_url)
 # If you aren't logged in, log in.
 elif driver.current_url == url or driver.current_url == 'https://caringbahhs.sentral.com.au/portal2/':
-    # Get 
+    # Get
     username = driver.find_element(By.ID, 'inputEmail')
     password = driver.find_element(By.ID, 'password')
     if (username.get_attribute("value") != usr):
@@ -58,5 +66,6 @@ elif driver.current_url == url or driver.current_url == 'https://caringbahhs.sen
     driver.find_element(By.XPATH, '//button[text()="Log In"]').click()
     time.sleep(5)
     if driver.current_url != 'https://caringbahhs.sentral.com.au/portal/dashboard':
-        raise TypeError("The URL is not at the specified Sentral dashboard.\n Please disable headless mode and test the code to ensure that it is functional.")
+        raise TypeError(
+            "The URL is not at the specified Sentral dashboard.\n Please disable headless mode and test the code to ensure that it is functional.")
     scrape_timetable(driver.page_source)
