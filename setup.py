dd461e0fd2ee0bba9a3aadd021bfb5ce8d35d777
J-J-B-J <maydays-twister0z@icloud.com>
2022-09-15 21:09:08 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,7 @@ from setuptools import setup
 setup(
     name="SentralTimetable",
     version="0.1",
-    description="The functions of Scratch brought to Python with Pygame.",
-    long_description="# Python-Scratch\nIs a Python library for creating "
-                     "Scratch projects.\nIt allows you to create a Scratch "
-                     "project in Python, using functions similar or "
-                     "identical to Scratch!",
+    description="A library to get your sentral timetable.",
     long_description_content_type="text/markdown",
     author="SuperHarmony910 and J-J-B-J",
     url="https://github.com/J-J-B-J/get-sentral-timetable",
