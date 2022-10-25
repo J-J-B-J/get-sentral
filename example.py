c5ed89ab206db4a2222248231677870375ef1a33
J-J-B-J <joshua.lea@icloud.com>
2022-10-25 16:08:01 +1100

    Update example.py, update Date().__str__()
    Date().__str__() now only includes the hour and minute if they have any value other than 0:00

--- example.py
+++ example.py
@@ -2,7 +2,7 @@
 Run this as an example to test out the code. If you have built and installed
 the package, you should be able to use it in another directory.
 """
-from SentralTimetable import get_timetable
+from SentralTimetable import get_timetable, objects
 import datetime
 
 
@@ -12,30 +12,11 @@ def __print_colour(text: any, hex_code: str):
     closest_colour = '30'
     closest_number = 16666651  # The greatest number possible for a difference
     # between two hex codes, plus one
-    hexes = [
-        '00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '0A', '0B',
-        '0C', '0D', '0E', '0F', '10', '11', '12', '13', '14', '15', '16', '17',
-        '18', '19', '1A', '1B', '1C', '1D', '1E', '1F', '20', '21', '22', '23',
-        '24', '25', '26', '27', '28', '29', '2A', '2B', '2C', '2D', '2E', '2F',
-        '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '3A', '3B',
-        '3C', '3D', '3E', '3F', '40', '41', '42', '43', '44', '45', '46', '47',
-        '48', '49', '4A', '4B', '4C', '4D', '4E', '4F', '50', '51', '52', '53',
-        '54', '55', '56', '57', '58', '59', '5A', '5B', '5C', '5D', '5E', '5F',
-        '60', '61', '62', '63', '64', '65', '66', '67', '68', '69', '6A', '6B',
-        '6C', '6D', '6E', '6F', '70', '71', '72', '73', '74', '75', '76', '77',
-        '78', '79', '7A', '7B', '7C', '7D', '7E', '7F', '80', '81', '82', '83',
-        '84', '85', '86', '87', '88', '89', '8A', '8B', '8C', '8D', '8E', '8F',
-        '90', '91', '92', '93', '94', '95', '96', '97', '98', '99', '9A', '9B',
-        '9C', '9D', '9E', '9F', 'A0', 'A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7',
-        'A8', 'A9', 'AA', 'AB', 'AC', 'AD', 'AE', 'AF', 'B0', 'B1', 'B2', 'B3',
-        'B4', 'B5', 'B6', 'B7', 'B8', 'B9', 'BA', 'BB', 'BC', 'BD', 'BE', 'BF',
-        'C0', 'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'CA', 'CB',
-        'CC', 'CD', 'CE', 'CF', 'D0', 'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7',
-        'D8', 'D9', 'DA', 'DB', 'DC', 'DD', 'DE', 'DF', 'E0', 'E1', 'E2', 'E3',
-        'E4', 'E5', 'E6', 'E7', 'E8', 'E9', 'EA', 'EB', 'EC', 'ED', 'EE', 'EF',
-        'F0', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'FA', 'FB',
-        'FC', 'FD', 'FE', 'FF'
-    ]
+    hex_system = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B',
+                  'C', 'D', 'E', 'F']
+    # Sets 'hexes' to ['00', '01', ... '09', '0A', ... '0F', '10', ... 'FF']
+    hexes = [a + b for a in hex_system for b in hex_system]
+
     colours = (
         '000000',  # Black
         'FF7F7F',  # Red
@@ -60,36 +41,48 @@ def __print_colour(text: any, hex_code: str):
 
 def main():
     """The main function"""
-    my_timetable = get_timetable()
+    timetable = get_timetable()
 
     print("\n\nCLASSES\n")
-    for my_period, my_class in my_timetable['classes'].items():
-        if my_class:
-            print(f'{my_period}: ', end='')
+    for class_ in timetable.classes:
+        if type(class_) == objects.Period:
+            class_: objects.Period
+            print(f'{class_.period}: ', end='')
             __print_colour(
-                f"{my_class['subject']} in {my_class['room']}"
-                f" with {my_class['teacher']}",
-                my_class['colour']
+                f"{class_.subject} in {class_.room}"
+                f" with {class_.teacher}",
+                class_.colour.hex
             )
-        else:
-            print(f"{my_period}: Empty")
+        elif type(class_) == objects.EmptyPeriod:
+            class_: objects.EmptyPeriod
+            print(f"{class_.period}: Empty")
 
     print("\n\nNOTICES\n")
-    for my_notice in my_timetable['notices']:
-        print(my_notice['title'].upper())
-        print('-' * len(my_notice['title']))
-        d = my_notice['date']
-        my_notice_date = datetime.datetime(d[0], d[1], d[2], d[3], d[4])\
-            .strftime('%a, %d %b at %I:%M%p')
-        print(f"By {my_notice['teacher']} on {my_notice_date}")
-        print('-' * len(my_notice['title']))
-        print(my_notice['content'] + '\n')
+    for notice in timetable.notices:
+        print(notice.title.upper())
+        print('-' * len(notice.title))
+        print(f"By {notice.teacher} on {notice.date}")
+        print('-' * len(notice.title))
+        print(notice.content + '\n')
 
     print("\nEVENTS\n")
-    for my_event in my_timetable['events']:
-        print(my_event['date'] + ': ' + my_event['name'].upper())
-        if my_event['flag']:
-            print("Flag: " + my_event['flag'])
+    for event in timetable.events:
+        print(str(event.date), end=': ')
+
+        if event.type_ == 'event':
+            print('\x1b[0;32m', end='')
+        elif event.type_ == 'assessment':
+            print('\x1b[0;31m', end='')
+
+        print(event.title.upper() + '\033[0;0m')
+        if event.flag:
+            print("Flag: ", end='')
+            try:
+                # Print with colour
+                print(f"\x1b[0;{int(event.flag.split()[1]) + 24}m", end='')
+            except ValueError:
+                pass
+            print(event.flag + '\033[0;0m')
         print()
 
 
