8e53a070fe95ce5722a0f3891fbc61c53609ffa9
J-J-B-J <joshua.lea@icloud.com>
2022-11-06 07:58:56 +1100

    Make events page

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import tkinter as tk
 from threading import Thread
 import SentralTimetable
 from functools import partial
+from calendar import Calendar
+import datetime
 
 
 class App:
@@ -26,7 +28,8 @@ class App:
         )
 
         self.state = 1
-        self.range_start = 0
+        self.notice_range_start = 0
+        self.event_range_start = 0
         self.reload()
 
     def create_buttons(self):
@@ -138,6 +141,14 @@ class App:
         self.destroy_section_objects()
         self.create_title("Timetable")
 
+        if not self.data.classes:
+            lbl_no_notices = tk.Label(
+                self.window,
+                text="No classes found."
+            )
+            self.section_objects.append(lbl_no_notices)
+            lbl_no_notices.pack()
+
         frm_timetable = tk.Frame(self.window, width=500, height=450)
         self.section_objects.append(frm_timetable)
         frm_timetable.pack()
@@ -184,18 +195,27 @@ class App:
         self.destroy_section_objects()
         self.create_title("Notices")
 
+        if not self.data.notices:
+            lbl_no_notices = tk.Label(
+                self.window,
+                text="No daily notices recorded for today. "
+                     "Please check back later."
+            )
+            self.section_objects.append(lbl_no_notices)
+            lbl_no_notices.pack()
+
         frm_notices = tk.Frame(self.window, width=500, height=450)
         self.section_objects.append(frm_notices)
         frm_notices.pack()
 
         def increase_range(*args):
             """Increase the range of notices shown"""
-            self.range_start += 5
+            self.notice_range_start += 5
             self.notices()
 
         def decrease_range(*args):
             """Decrease the range of notices shown"""
-            self.range_start -= 5
+            self.notice_range_start -= 5
             self.notices()
 
         def open_notice(notice: SentralTimetable.Notice, event: tk.Event):
@@ -234,7 +254,7 @@ class App:
 
             notice_window.mainloop()
 
-        for notice in self.data.notices[self.range_start:self.range_start + 5]:
+        for notice in self.data.notices[self.notice_range_start:self.notice_range_start + 5]:
             frm_notice = tk.Frame(frm_notices, width=500, height=50)
             self.section_objects.append(frm_notice)
             frm_notice.pack()
@@ -262,7 +282,7 @@ class App:
         )
         self.section_objects.append(btn_increase_range)
         btn_increase_range.pack(side=tk.RIGHT)
-        if self.range_start + 5 < len(self.data.notices):
+        if self.notice_range_start + 5 < len(self.data.notices):
             btn_increase_range.bind("<Button-1>", increase_range)
         else:
             btn_increase_range.config(state=tk.DISABLED)
@@ -274,7 +294,7 @@ class App:
         )
         self.section_objects.append(btn_decrease_range)
         btn_decrease_range.pack(side=tk.LEFT)
-        if self.range_start > 0:
+        if self.notice_range_start > 0:
             btn_decrease_range.bind("<Button-1>", decrease_range)
         else:
             btn_decrease_range.config(state=tk.DISABLED)
@@ -283,6 +303,196 @@ class App:
         """The 'timetable' page"""
         self.destroy_section_objects()
         self.create_title("Events")
+
+        if not self.data.events:
+            lbl_no_events = tk.Label(
+                self.window,
+                text="No events found."
+            )
+            self.section_objects.append(lbl_no_events)
+            lbl_no_events.pack()
+            return
+
+        frm_calendar = tk.Frame(self.window, width=500, height=450)
+        self.section_objects.append(frm_calendar)
+        frm_calendar.pack()
+
+        start_month = self.data.events[0].date.month
+        start_year = self.data.events[0].date.year
+        end_month = self.data.events[-1].date.month
+        end_year = self.data.events[-1].date.year
+
+        cal = Calendar(6)
+        months = [
+            cal.monthdatescalendar(y, m)
+            for m in range(start_month, end_month + 1)
+            for y in range(start_year, end_year + 1)
+        ]
+        month = months[self.event_range_start]
+
+        lbl_month = tk.Label(
+            frm_calendar,
+            text=month[2][0].strftime("%B %Y"),
+            font=("Arial", 20),
+            width=50,
+            height=2
+        )
+        self.section_objects.append(lbl_month)
+        lbl_month.pack()
+
+        def increase_range(*args):
+            """Increase the range of events shown"""
+            self.event_range_start += 1
+            self.events()
+
+        def decrease_range(*args):
+            """Decrease the range of events shown"""
+            self.event_range_start -= 1
+            self.events()
+
+        def open_event(event: SentralTimetable.Event, tk_event: tk.Event):
+            """Show the detail view for an event"""
+            event_window = tk.Tk()
+            event_window.title('Event')
+            event_window.geometry('500x150')
+            event_window.focus_set()
+            event_window.bind(
+                "<Escape>",
+                lambda *args: event_window.destroy()
+            )
+
+            lbl_title = tk.Label(
+                event_window,
+                text=event.title,
+                font=("Arial", 20),
+                wraplength=490
+            )
+            lbl_title.pack(side=tk.TOP)
+
+            if type(event.date) == SentralTimetable.Date:
+                if event.date.hour == 0 and event.date.minute == 0:
+                    lbl_date = tk.Label(
+                        event_window,
+                        text=f"On {event.date.day}/{event.date.month}/"
+                             f"{event.date.year}"
+                    )
+                else:
+                    lbl_date = tk.Label(
+                        event_window,
+                        text=f"On {event.date.day}/{event.date.month}/"
+                             f"{event.date.year} at {event.date.hour}:"
+                             f"{str(event.date.minute).rjust(2, '0')}"
+                    )
+            else:
+                lbl_date = tk.Label(
+                    event_window,
+                    text=f"On {event.date.day}/{event.date.month}/"
+                         f"{event.date.year} from {event.date.start_hour}:"
+                         f"{str(event.date.start_minute).rjust(2, '0')} to "
+                         f"{event.date.end_hour}:"
+                         f"{str(event.date.end_minute).rjust(2, '0')}"
+                )
+            lbl_date.pack(side=tk.TOP)
+
+            if event.flag:
+                lbl_flag = tk.Label(
+                    event_window,
+                    text=f"Flag: {event.flag}",
+                    wraplength=490
+                )
+                lbl_flag.pack(side=tk.TOP)
+
+            lbl_type = tk.Label(
+                event_window,
+                text=f"Type: {event.type_}",
+                wraplength=490
+            )
+            lbl_type.pack(side=tk.TOP)
+
+            event_window.mainloop()
+
+        def open_day(day: datetime.date, tk_event: tk.Event):
+            """Show the events for a day"""
+            day_window = tk.Tk()
+            day_window.title(day.strftime("%A %d %B %Y"))
+            day_window.geometry('500x150')
+            day_window.focus_set()
+            day_window.bind(
+                "<Escape>",
+                lambda *args: day_window.destroy()
+            )
+
+            lbl_title = tk.Label(
+                day_window,
+                text=f"Events on {day.day}/{day.month}/{day.year}",
+                font=("Arial", 20)
+            )
+            lbl_title.pack(side=tk.TOP)
+
+            for event in self.data.events:
+                if (event.date.dy, event.date.mth, event.date.yr) == \
+                        (day.day, day.month, day.year):
+                    lbl_event = tk.Label(
+                        day_window,
+                        text=event.title,
+                        wraplength=490
+                    )
+                    lbl_event.pack(side=tk.TOP)
+                    lbl_event.bind(
+                        "<Button-1>",
+                        partial(open_event, event)
+                    )
+
+            day_window.mainloop()
+
+        for week in month:
+            frm_week = tk.Frame(frm_calendar, width=500, height=50)
+            self.section_objects.append(frm_week)
+            frm_week.pack()
+
+            for day in week:
+                lbl_day = tk.Label(
+                    frm_week,
+                    text=day.day,
+                    width=4,
+                    height=2,
+                    borderwidth=3
+                )
+                self.section_objects.append(lbl_day)
+                lbl_day.pack(side=tk.LEFT)
+
+                if day.month == month[2][0].month:
+                    lbl_day.config(relief="groove")
+                    lbl_day.bind(
+                        "<Button-1>",
+                        partial(open_day, day)
+                    )
+                else:
+                    lbl_day.config(state=tk.DISABLED)
+
+        btn_increase_range = tk.Button(
+            frm_calendar,
+            text=">",
+            width=10
+        )
+        self.section_objects.append(btn_increase_range)
+        btn_increase_range.pack(side=tk.RIGHT)
+        if self.event_range_start + 1 < len(months):
+            btn_increase_range.bind("<Button-1>", increase_range)
+        else:
+            btn_increase_range.config(state=tk.DISABLED)
+
+        btn_decrease_range = tk.Button(
+            frm_calendar,
+            text="<",
+            width=10
+        )
+        self.section_objects.append(btn_decrease_range)
+        btn_decrease_range.pack(side=tk.LEFT)
+        if self.event_range_start > 0:
+            btn_decrease_range.bind("<Button-1>", decrease_range)
+        else:
+            btn_decrease_range.config(state=tk.DISABLED)
 
     def me(self, *args):
         """The 'me' page"""
